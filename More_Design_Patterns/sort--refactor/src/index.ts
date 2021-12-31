f58bb1bef20332415f261885fdce33b3101b6281
Om <hungomoct19@outlook.com>
2021-12-31 18:23:12 +0800

    abstract vs interface - side-notes

--- More_Design_Patterns/sort--refactor/src/index.ts
+++ More_Design_Patterns/sort--refactor/src/index.ts
@@ -28,3 +28,15 @@ linkedList.print();
 // Note  =>
 // 1) Typeof - number, string,boolean, symbol
 // 2) instanceof  - every other value that is created with a constructor function
+
+
+// ========= interface vs abstract/inhritance ========
+// interface 
+    // 1)set up a contract between different classes 
+    // 2) use when we have very differnt objects that we want ot work together
+    // 3) Pormote loose coupling
+
+//Abstract class/Inheritnace
+    // 1) Sets up a contract between differnt classes
+    // 2) Use when we are trying to build up a definition of an object
+    // 3) strongly couples classes together
