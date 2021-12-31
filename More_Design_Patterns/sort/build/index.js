2419c28d05f0e00fe444a5353be0e3a5f606dabd
Om <hungomoct19@outlook.com>
2021-12-31 13:49:45 +0800

    basic number sorting implementation

--- More_Design_Patterns/sort/build/index.js
+++ More_Design_Patterns/sort/build/index.js
@@ -1,6 +1,27 @@
 "use strict";
-console.log('Hello, there');
-const logSomthing = () => {
-    console.log("This is crazy");
-};
-logSomthing();
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        let sorted = this.collection;
+        const { length } = this.collection;
+        for (let x = 0; x < length; x++) {
+            for (let y = 0; y < length - 1 - x; y++) {
+                const [currentIndex, nextIndex] = [y, y + 1];
+                if (sorted[currentIndex] > sorted[nextIndex]) {
+                    this.swap(currentIndex, nextIndex, sorted);
+                }
+            }
+        }
+        this.collection = sorted;
+    }
+    swap(a, b, arr) {
+        arr[a] = arr[a] + arr[b];
+        arr[b] = arr[a] - arr[b];
+        arr[a] = arr[a] - arr[b];
+    }
+}
+const sorter = new Sorter([10, -3, 23, 3, 4, 5, 7, 2, 1]);
+sorter.sort();
+console.log(sorter.collection);
--- More_Design_Patterns/sort/src/index.ts
+++ More_Design_Patterns/sort/src/index.ts
@@ -1,7 +1,30 @@
-console.log('Hello, there')
-
-const logSomthing = ():void=>{
-    console.log("This is crazy")
-};
-
-logSomthing();
+class Sorter {
+    collection: number[];
+    constructor(collection: number[]) {
+        this.collection = collection
+    }
+    sort(): void {
+        let sorted: number[] = this.collection;
+        const {
+            length
+        } = this.collection
+        for (let x = 0; x < length; x++) {
+            for (let y = 0; y < length - 1 - x; y++) {
+                const [currentIndex, nextIndex] = [y, y + 1];
+                if (sorted[currentIndex] > sorted[nextIndex]) {
+                    this.swap(currentIndex, nextIndex, sorted);
+                }
+           
+              }
+        }
+        this.collection = sorted;
+    }
+    private swap(a: number, b: number,arr:number[]): void {
+        arr[a] = arr[a] + arr[b];
+        arr[b] = arr[a] - arr[b];
+        arr[a] = arr[a] - arr[b];
+    }
+}
+const sorter = new Sorter([10, -3, 23, 3, 4, 5, 7, 2, 1])
+sorter.sort()
+console.log(sorter.collection)
