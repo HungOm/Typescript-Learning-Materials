9def691f96a77ee5fa8c1cc25c7280a34e5d1fc3
Om <hungomoct19@outlook.com>
2022-01-03 01:13:20 +0800

    index file clean up

--- CodeReuse/refactor_object_composition/src/index.ts
+++ CodeReuse/refactor_object_composition/src/index.ts
@@ -1,20 +1,10 @@
 
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { WinAnalysis } from './analyzers/WinAnalysis';
 import { Summary } from './summary';
-
-import { HtmlReport } from './reportTargets/HtmlReport';
 
 const csv_reader = MatchReader.fromCsv('./../football.csv');
 csv_reader.load();
 
-
-// const summary = new Summary(
-//     new WinAnalysis('Man United'),
-//     new HtmlReport()
-// )
 // static allow class to access without creating instance - e.g new Summary()
 const summary = Summary.winAnalysisHtmlReport('Man United')
 summary.buildAndPrintReport(csv_reader.matches)
